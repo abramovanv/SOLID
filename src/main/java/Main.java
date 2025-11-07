47626c238c489f2388d3e9041d0e2624365cea5a
abramovanv <38113082+abramovanv@users.noreply.github.com>
2025-11-07 07:31:00 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,7 @@
 import java.time.LocalDate;
 import java.util.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         final int MAX_CAPACITY = 3;
